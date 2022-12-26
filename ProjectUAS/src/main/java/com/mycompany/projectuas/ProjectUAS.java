37903ace40c2ffe52934b4ae4463fc5635ebb738
62821 <62821@Farhan>
2022-12-26 15:19:17 +0700

    pertama

---
+++ ProjectUAS/src/main/java/com/mycompany/projectuas/ProjectUAS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectuas;
+
+/**
+ *
+ * @author 62821
+ */
+public class ProjectUAS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
