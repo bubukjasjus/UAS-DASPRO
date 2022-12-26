fca14495b37db44a75f59a7ee4d01028f7cf519e
62821 <62821@Farhan>
2022-12-26 15:12:07 +0700

    berkah

---
+++ BerkahRentalKendaraan/src/main/java/com/mycompany/berkahrentalkendaraan/BerkahRentalKendaraan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.berkahrentalkendaraan;
+
+/**
+ *
+ * @author 62821
+ */
+public class BerkahRentalKendaraan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
